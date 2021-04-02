db3337c7a7e7aec7d39d689f79c4905beb4ee5d2
Nathan Tang <nathan.ly.tang@gmail.com>
2021-04-01 22:57:51 -0600

    Init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-unused-vars": 0
+    }
+};
