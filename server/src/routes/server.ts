45e76e77885121c0710fa3e967d2ace4fcff9074
Nathan Tang <nathan.ly.tang@gmail.com>
2021-04-07 23:26:21 -0600

    Add types

--- server/src/routes/server.ts
+++ server/src/routes/server.ts
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const status = require('../services/status');
 
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', async function(req: any, res: any, next) {
     const { id } = req.params;
     let api = await status.getStatus(id);
-    console.log('serverapi: ', api)
+    console.log(api)
     res.send(api);
 });
 
