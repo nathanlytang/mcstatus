f3448afc905e8ebc423f72915a154a5415186864
Nathan Tang <nathan.ly.tang@gmail.com>
2021-05-14 03:02:42 -0600

    Feat: Add return status

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import InputForm from "./components/form";
+import ReturnStatus from "./components/returnStatus"
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { apiResponse: "", address: "" };
     }
 
-    callAPI() {
-        fetch("http://localhost:9000/testAPI")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
+    getAddress = (data) => {
+        this.setState({ address: data })
     }
 
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    <h1 className="App-title">Server Status</h1>
                 </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+
+                <InputForm largeInput={"Get Status"} smallInput={"→"} placeholder={"Server address"} parentCallback={this.getAddress} />
+                <ReturnStatus address={this.state.address} />
+
+                {/* <p className="App-intro">{this.state.apiResponse}</p> */}
             </div>
         );
     }
