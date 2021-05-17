ae4bfedc44349decb5d3ef43e305349192654bc4
Nathan Tang <nathan.ly.tang@gmail.com>
2021-05-17 00:00:15 -0600

    Feat: collapsible technical info

---
+++ app/src/components/collapse.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import "../App.scss"
+
+class Collapsible extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState(state => ({
+            open: !state.open
+        }));
+        document.getElementById("description").classList.add("animate");
+    }
+
+    titlePosition(title, openButton, closeButton, position) {
+        if (position === "left") {
+            return (
+                <div>
+                    <span id="arrow">{this.state.open ? closeButton : openButton}</span>
+                    <span id="title">{title}</span>
+                </div>
+            );
+        } else if (position === "right") {
+            return (
+                <div>
+                    <span id="title">{title}</span>
+                    <span id="arrow">{this.state.open ? closeButton : openButton}</span>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        const title = this.props.title;
+        const openButton = this.props.openButton;
+        const closeButton = this.props.closeButton;
+        const position = this.props.buttonPosition
+
+        return (
+            <div className="collapsible">
+                <button className="active" onClick={this.toggle}>
+                    {this.titlePosition(title, openButton, closeButton, position)}
+                </button>
+                <div className="description animate animate.grow">
+                    {this.state.open &&
+                        <div>
+                            {this.props.children}
+                        </div>
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Collapsible;
